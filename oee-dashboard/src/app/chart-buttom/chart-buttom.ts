6bb556c98d8b1a76b36c8d68e994f2aacc9e8112
phhorsap <43028614+phhorsap@users.noreply.github.com>
2025-09-17 10:30:20 +0000

    topNchart

--- oee-dashboard/src/app/chart-buttom/chart-buttom.ts
+++ oee-dashboard/src/app/chart-buttom/chart-buttom.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 
+import { MatIconModule } from '@angular/material/icon';
+
+
 @Component({
   selector: 'app-chart-buttom',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './chart-buttom.html',
   styleUrl: './chart-buttom.scss'
 })
 export class ChartButtom {
+  oeeTopN=[{product:'Book',total:5422,psc: 250},
+    {product:'Bike',total:4422,psc: 50},
+    
+
+  ]
 
 }
